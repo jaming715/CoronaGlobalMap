d8678cd53a68b959f89f91e1a7311d2bc7905a15
Josean Martinez <josean@josean.com>
2020-03-21 01:56:23 -0400

    Removed access to deleting and posting to DB

--- routes/province-router.js
+++ routes/province-router.js
@@ -2,28 +2,28 @@ const express = require('express');
 const router = express.Router();
 const {Province} = require('../models/province-model.js');
 
-router.get('/', async (req, res) => {
-  try {
-    const provinces = await Province.find();
-    res.send(provinces);
-  } catch (err) {
-    console.log('Error retrieving categories', err);
-  }
-});
+// router.get('/', async (req, res) => {
+//   try {
+//     const provinces = await Province.find();
+//     res.send(provinces);
+//   } catch (err) {
+//     console.log('Error retrieving categories', err);
+//   }
+// });
 
-router.post('/', async (req, res) => {
-  const province = req.body;
-  try {
-    const doc = await Country.create({
-      name: province.name,
-      totCases: province.totCases,
-      totDeaths: province.totDeaths,
-      totRecovered: province.totRecovered,
-    });
-    res.send(doc);
-  } catch (err) {
-    console.log('Error adding country.', err);
-  }
-});
+// router.post('/', async (req, res) => {
+//   const province = req.body;
+//   try {
+//     const doc = await Country.create({
+//       name: province.name,
+//       totCases: province.totCases,
+//       totDeaths: province.totDeaths,
+//       totRecovered: province.totRecovered,
+//     });
+//     res.send(doc);
+//   } catch (err) {
+//     console.log('Error adding country.', err);
+//   }
+// });
 
 module.exports = router;
